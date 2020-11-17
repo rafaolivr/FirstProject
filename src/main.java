7b4a778747663e9070e4a1ef9af482b5a78a7bc1
Rafael Oliveira <rafaelolivr16@gmail.com>
2020-11-16 21:57:40 -0300

    teste

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("TESTE");
         System.out.println("teste2");
+        System.out.println("teste3");
     }
 
 }
